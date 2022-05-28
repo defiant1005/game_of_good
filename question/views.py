382183497c8097ed765c3f975144a071bfe1f718
rinat.idiiatov <rinat.idiiatov@qlever.com>
2022-05-28 19:16:04 +0300

    pass

--- question/views.py
+++ question/views.py
@@ -1,12 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from question.models import Question
 from question.serializers import QuestionSerializer
 
 
 class QuestionViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated, ]
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
